38b6c6958753eb2b55b501cefbe207938e4731ca
Anthony GOCMEN <anthony.gocmen@gmail.com>
2025-08-18 19:02:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // dépôt = ag-algolab.github.io → base racine
+  base: '/', // repo = ag-algolab.github.io
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // alias @ -> /src
+    },
+  },
 })
