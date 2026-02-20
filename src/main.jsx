efc76c16dc98b3da74c1719b30cd9c1b03f15bc8
Anthony GOCMEN <anthony.gocmen@gmail.com>
2026-02-20 23:06:11 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from '@/App';
 import '@/index.css';
+import ScrollToTop from './components/ScrollToTop';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
